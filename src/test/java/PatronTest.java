f4271c3a9fd013eb4fd1cbbb8751ca74bf76a723
austin minnon and jake steinberg <jakesteinberg@gmail.com>
2016-03-02 11:23:31 -0800

    add patron tests for saving and etc

--- src/test/java/PatronTest.java
+++ src/test/java/PatronTest.java
@@ -1,12 +1,83 @@
-import java.util.Arrays;
-import java.util.List;
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.List;
 
 public class PatronTest {
 
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Patron.all().size(), 0);
+  }
 
+  @Test
+  public void equals_returnsTrueIfDescriptionsAretheSame() {
+    Patron firstPatron = new Patron("Jim");
+    Patron secondPatron = new Patron("Jim");
+    assertTrue(firstPatron.equals(secondPatron));
+  }
+
+  @Test
+  public void save_savesObjectIntoDatabase() {
+    Patron firstPatron = new Patron("Jim");
+    firstPatron.save();
+    Patron savedPatron = Patron.all().get(0);
+    assertTrue(savedPatron.equals(firstPatron));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Patron firstPatron = new Patron("Jim");
+    firstPatron.save();
+    Patron savedPatron = Patron.all().get(0);
+    assertEquals(firstPatron.getId(), savedPatron.getId());
+  }
+  @Test
+  public void find_findPatronInDatabase_true() {
+    Patron firstPatron = new Patron("Jim");
+    firstPatron.save();
+    Patron savedPatron = Patron.find(firstPatron.getId());
+    assertTrue(firstPatron.equals(savedPatron));
+  }
+//
+//   @Test
+//   public void addCourse_addsCourseToStudent() {
+//     Course myCourse = new Course("eco101");
+//     myCourse.save();
+//
+//     Student myStudent = new Student("Jimmy", "01.01.2016");
+//     myStudent.save();
+//
+//     myStudent.addCourse(myCourse);
+//     Course savedCourse = myStudent.getCourses().get(0);
+//     assertTrue(myCourse.equals(savedCourse));
+// }
+//
+//   @Test
+//   public void getCourses_returnsAllCourses_List() {
+//     Course myCourse = new Course("eco101");
+//     myCourse.save();
+//
+//     Student myStudent = new Student("Jimmy", "01.01.2016");
+//     myStudent.save();
+//
+//     myStudent.addCourse(myCourse);
+//     List savedCourses = myStudent.getCourses();
+//     assertEquals(savedCourses.size(), 1);
+//   }
+//
+//   @Test
+//   public void delete_deletesAllStudentsAndCourseAssoications() {
+//     Course myCourse = new Course("eco101");
+//     myCourse.save();
+//
+//     Student myStudent = new Student("Jimmy", "01.01.2016");
+//     myStudent.save();
+//
+//     myStudent.addCourse(myCourse);
+//     myStudent.delete();
+//     assertEquals(myCourse.getStudents().size(), 0);
+//   }
 }
